d80842117b65ffb16c671b096114543aa83c13f3
CuriousLocky <835322750jm37@outlook.com>
2021-05-04 17:52:21 +0900

    Add form cluster function (in plaintext mode)

--- app/src/main/java/mpc/project/ManagerMain.java
+++ app/src/main/java/mpc/project/ManagerMain.java
@@ -1,8 +1,61 @@
 package mpc.project;
 
+import com.google.protobuf.ByteString;
+import io.grpc.*;
+import io.grpc.stub.StreamObserver;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class ManagerMain {
+    final int clusterMaxSize = 48;
+    final int keyBitLength = 1024;
+    private Random rnd;
+    private Server server;
+    private int portNum;
+    private int id;
+    private BigInteger P;
+    ArrayList<WorkerServiceGrpc.WorkerServiceStub> stubs;
+    private boolean formCluster(String target, int workerId){
+        System.out.println("verifying validity of " + target);
+        Channel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+        WorkerServiceGrpc.WorkerServiceBlockingStub testStub = WorkerServiceGrpc.newBlockingStub(channel);
+        StdRequest req = StdRequest.newBuilder()
+                .setId(workerId)
+                .setContents(ByteString.copyFrom(P.toByteArray()))
+                .build();
+        StdResponse res;
+        try{
+            res = testStub.formCluster(req);
+        }catch(StatusRuntimeException e){
+            System.out.println("Failed to add into cluster: "+e.getMessage());
+            return false;
+        }
+        stubs.add(WorkerServiceGrpc.newStub(channel));
+        System.out.println(target + " is registered successfully");
+        return true;
+    }
     public ManagerMain(int portNum){
+        this.portNum = portNum;
+        this.rnd = new Random();
+        this.P = BigInteger.probablePrime(keyBitLength, rnd);
+        System.out.println("please enter the address:port of all workers");
+        System.out.println("one in each line, \"end\" marks the end");
+        Scanner input = new Scanner(System.in);
+        stubs = new ArrayList<WorkerServiceGrpc.WorkerServiceStub>();
+        for(int i = 1; i < clusterMaxSize; i++){
+            String inLine = input.nextLine();
+            if(inLine.equals("end")){
+                break;
+            }
+            if(!formCluster(inLine, i)){
+                i--;
+            }
+        }
+    }
+    public void run(){
 
     }
-    public void run(){}
 }
--- app/src/main/java/mpc/project/WorkerMain.java
+++ app/src/main/java/mpc/project/WorkerMain.java
@@ -1,14 +1,46 @@
 package mpc.project;
 
+import com.google.protobuf.ByteString;
 import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.math.BigInteger;
 
 public class WorkerMain {
-    private Server workerServer;
+    private Server server;
     private int portNum;
+    private int id;
+    private BigInteger P;
+    class WorkerServiceImpl extends WorkerServiceGrpc.WorkerServiceImplBase{
+        @Override
+        public void formCluster(StdRequest req, StreamObserver<StdResponse> resObserver){
+            id = req.getId();
+            P = new BigInteger(req.getContents().toByteArray());
+            StdResponse res = StdResponse.newBuilder().setId(id).build();
+            resObserver.onNext(res);
+            resObserver.onCompleted();
+            System.out.println("connected to Manager");
+        }
+    }
     public WorkerMain(int portNum){
         this.portNum = portNum;
     }
     public void run(){
-
+        try{
+            this.server = ServerBuilder.forPort(portNum)
+                    .addService(new WorkerServiceImpl())
+                    .build().start();
+            System.out.println("Waiting for manager to connect");
+            this.server.awaitTermination();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            System.exit(-2);
+        }finally {
+            if(server!=null){
+                server.shutdownNow();
+            }
+        }
     }
 }
