5ca623dce43a252970f607a61d89b69cbafb1807
7942jun <7942jun@gmail.com>
2021-05-06 00:30:24 +0900

    Add block-by encryption and decryption

--- src_tmp/RSAEncryptedData.java
+++ src_tmp/RSAEncryptedData.java
@@ -8,42 +8,64 @@ public class RSAEncryptedData {
     /** Todo:
      *    Implement encryption and decryption methods
      **/
-//    private final int blockSize = 8
+
+    /** Encryption: Encrypt block by block( blockSize bytes -> encBlockSize bytes )
+     *   Decryption: Decrypt block by block( encBlockSize bytes -> blockSize bytes )
+     **/
+    private final int blockSize = 2; /** blockSize * 8 should be under log_2(N) **/
+    private final int encBlockSize = 3; /** endBlockSize * 8 should be over log_2(N) **/
+    private final int base64BlockSize = (encBlockSize + 2) / 3 * 4; /** considering Base64 padding **/
 
     public RSAEncryptedData() {}
     public RSAEncryptedData(String string, Key key){
     }
 
     public String encrypt(String string, Key key) {
-        byte[] byteString = string.getBytes();
-        BigInteger b = new BigInteger(byteString);
-        b = b.modPow(key.e, key.N);
-        byte[] decrypted = b.toByteArray();
-        // Base64 encoding
+        // Encoder for Base64 encoding
         Base64.Encoder encoder = Base64.getEncoder();
-        return new String(encoder.encode(decrypted));
+
+        String result = "";
+        byte[] byteString = string.getBytes();
+        // block-by encoding
+        for(int i = 0; i < byteString.length; i += blockSize) {
+            byte[] block = Arrays.copyOfRange(byteString, i, Math.min(byteString.length, i + blockSize));
+            BigInteger b = new BigInteger(block);
+
+            // RSA calculation
+            b = b.modPow(key.e, key.N);
+            byte[] bByte = b.toByteArray();
+
+            byte[] encBlock = new byte[encBlockSize];
+            System.arraycopy(b.toByteArray(), 0, encBlock, encBlockSize - bByte.length, bByte.length);
+            result += encoder.encodeToString(encBlock);
+        }
+
+        return result;
     }
 
     public String decrypt(String string, Key key) {
-        // Base64 decoding
+        // Decoder for Base64 decoding
         Base64.Decoder decoder = Base64.getDecoder();
-        byte[] byteString = decoder.decode(string.getBytes());
 
-        BigInteger b = new BigInteger(byteString);
-        b = b.modPow(key.d, key.N);
-        byte[] decrypted = b.toByteArray();
-        return new String(decrypted);
+        String result = "";
+        byte[] byteString = string.getBytes();
+        // block-by decoding
+        for(int i = 0; i < byteString.length; i += base64BlockSize) {
+            byte[] block = Arrays.copyOfRange(byteString, i, i + base64BlockSize);
+            block = decoder.decode(block);
+            BigInteger b = new BigInteger(block);
 
-//        for(int i = 0; i < string.length(); i += blockSize) {
-//            byte[] block = Arrays.copyOfRange(byteString, i, i + blockSize);
-//            BigInteger b = new BigInteger(block);
-//            b = BigInteger.ModPow(b, key.d, key.N);
-//
-//            byte[] decrypted = b.toByteArray(isUnsigned=true);
-//            System.arraycopy(decrypted, 0, byteString, blockSize - decrypted.length(), decrypted.length());
-//            Arrays.fill(byteString, i, blockSize - decrypted.length(), 0);
-//
-//            return new String(byteString)
-//        }
+            // RSA calculation
+            b = b.modPow(key.d, key.N);
+            byte[] bByte = b.toByteArray();
+
+            /**
+             * Todo: padding result with another block size
+             * If this method is used for distributed decryption, another block size is needed.
+             */
+            result += new String(bByte);
+        }
+
+        return result;
     }
 }
