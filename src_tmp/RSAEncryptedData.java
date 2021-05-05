ea32d1325dd3dbadf7260d52c01374b302052324
7942jun <7942jun@gmail.com>
2021-05-06 01:35:16 +0900

    Add distributed decrytion method

--- src_tmp/RSAEncryptedData.java
+++ src_tmp/RSAEncryptedData.java
@@ -12,8 +12,8 @@ public class RSAEncryptedData {
     /** Encryption: Encrypt block by block( blockSize bytes -> encBlockSize bytes )
      *   Decryption: Decrypt block by block( encBlockSize bytes -> blockSize bytes )
      **/
-    private final int blockSize = 2; /** blockSize * 8 should be under log_2(N) **/
-    private final int encBlockSize = 3; /** endBlockSize * 8 should be over log_2(N) **/
+    private final int blockSize = 1; /** blockSize * 8 should be under log_2(N) **/
+    private final int encBlockSize = 2; /** endBlockSize * 8 should be over log_2(N) **/
     private final int base64BlockSize = (encBlockSize + 2) / 3 * 4; /** considering Base64 padding **/
 
     public RSAEncryptedData() {}
@@ -43,9 +43,16 @@ public class RSAEncryptedData {
         return result;
     }
 
-    public String decrypt(String string, Key key) {
-        // Decoder for Base64 decoding
+    /**
+     * @param dist : using for distributed Decryption
+     *             if true, calculate (m^di Mod N) and base64 encoding to send to manager.
+     *             if false, calculate (m^d Mod N). The result is original message.
+     *             This is for testing
+     */
+    public String decrypt(String string, Key key, boolean dist) {
+        // Encoder and Decoder for Base64 decoding
         Base64.Decoder decoder = Base64.getDecoder();
+        Base64.Encoder encoder = Base64.getEncoder();
 
         String result = "";
         byte[] byteString = string.getBytes();
@@ -59,13 +66,51 @@ public class RSAEncryptedData {
             b = b.modPow(key.d, key.N);
             byte[] bByte = b.toByteArray();
 
-            /**
-             * Todo: padding result with another block size
-             * If this method is used for distributed decryption, another block size is needed.
-             */
-            result += new String(bByte);
+            if(dist) {
+                /** calculate (m^di Mod N) and base64 encoding to send to manager **/
+                byte[] encBlock = new byte[encBlockSize];
+                System.arraycopy(b.toByteArray(), 0, encBlock, encBlockSize - bByte.length, bByte.length);
+                result += encoder.encodeToString(encBlock);
+            } else {
+                /** simple RSA decryption **/
+                result += new String(bByte);
+            }
         }
 
+        return result;
+    }
+
+    /**
+     * @param strings: decrypted messages with each di
+     *               strings should have same length
+     * @param key: any Key. just used for mod N calculation
+     **/
+    public String distributedDecrypt(String[] strings, Key key) {
+        // Decoder for Base64 decoding
+        Base64.Decoder decoder = Base64.getDecoder();
+
+        byte[][] byteStrings = new byte[strings.length][];
+        for(int i = 0; i < strings.length; ++i) {
+            byteStrings[i] = strings[i].getBytes();
+        }
+
+        String result = "";
+        int len = byteStrings[0].length;
+        for(int i = 0; i < len; i += base64BlockSize) {
+            BigInteger b = BigInteger.valueOf(1);
+
+            for(int j = 0; j < strings.length; ++j) {
+                byte[] block = Arrays.copyOfRange(byteStrings[j], i, Math.min(len, i + base64BlockSize));
+                block = decoder.decode(block);
+                BigInteger b_j = new BigInteger(block);
+
+                // Distributed decryption
+                b = b.multiply(b_j).mod(key.N);
+            }
+
+            byte[] bByte = b.toByteArray();
+            result += new String(bByte);
+        }
         return result;
     }
 }
