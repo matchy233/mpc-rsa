552c0f96eb538e9d7296ba7fd3044c0ce2e75025
matchy233 <matchy233@gmail.com>
2021-05-22 22:16:53 +0900

    Reformat RPC utility structure

--- app/src/main/java/mpc/project/util/RpcUtility.java
+++ app/src/main/java/mpc/project/util/RpcUtility.java
@@ -7,51 +7,79 @@ import mpc.project.StdResponse;
 import mpc.project.ExchangePrimespqhRequest;
 
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 
 public class RpcUtility {
-    static public StdRequest newStdRequest(int id, BigInteger bigInt) {
-        StdRequest result = StdRequest.newBuilder()
-                .setId(id)
-                .setContents(ByteString.copyFrom(bigInt.toByteArray()))
-                .build();
-        return result;
+
+    static public class Request {
+        /*
+         * General purpose request
+         *      Can send int, String or BigInteger
+         */
+        static public StdRequest newStdRequest(int id, BigInteger bigInt) {
+            return StdRequest.newBuilder()
+                    .setId(id)
+                    .setContents(ByteString.copyFrom(bigInt.toByteArray()))
+                    .build();
+        }
+
+        static public StdRequest newStdRequest(int id, String s) {
+            return StdRequest.newBuilder()
+                    .setId(id)
+                    .setContents(ByteString.copyFrom(s.getBytes()))
+                    .build();
+        }
+
+        static public StdRequest newStdRequest(int id) {
+            return StdRequest.newBuilder().setId(id).build();
+        }
+
+        /*
+         * Request for generating modulus N
+         *    Exchange prime number p, q and h (all BigInteger)
+         */
+        static public ExchangePrimespqhRequest newExchangePrimesRequest(int id, BigInteger p, BigInteger q, BigInteger h) {
+            return ExchangePrimespqhRequest.newBuilder()
+                    .setId(id)
+                    .setP(ByteString.copyFrom(p.toByteArray()))
+                    .setQ(ByteString.copyFrom(q.toByteArray()))
+                    .setH(ByteString.copyFrom(h.toByteArray()))
+                    .build();
+        }
     }
 
-    static public StdRequest newStdRequest(int id, String s) {
-        StdRequest result = StdRequest.newBuilder()
-                .setId(id).setContents(ByteString.copyFrom(
-                        s.getBytes()
-                )).build();
-        return result;
+
+    static public class Response {
+        /*
+         * General purpose response
+         */
+        static public StdResponse newStdResponse(int id) {
+            return StdResponse.newBuilder().setId(id).build();
+        }
+
+        static public StdResponse newStdResponse(int id, String s) {
+            return StdResponse.newBuilder()
+                    .setId(id)
+                    .setContents(ByteString.copyFrom(s.getBytes()))
+                    .build();
+        }
+
+        /*
+         * Response for primality test
+         *     Send verification numbers
+         */
+        static public PrimalityTestResponse newPrimalityTestResponse(int id) {
+            return PrimalityTestResponse.newBuilder().setId(id).build();
+        }
+
+        static public PrimalityTestResponse newPrimalityTestResponse(int id, BigInteger v) {
+            PrimalityTestResponse result = PrimalityTestResponse.newBuilder()
+                    .setId(id)
+                    .setV(ByteString.copyFrom(v.toByteArray()))
+                    .build();
+            return result;
+        }
+
     }
 
-    static public StdRequest newStdRequest(int id) {
-        return StdRequest.newBuilder().setId(id).build();
-    }
-
-    static public ExchangePrimespqhRequest newExchangePrimesRequest(int id, BigInteger p, BigInteger q, BigInteger h) {
-        ExchangePrimespqhRequest result = ExchangePrimespqhRequest.newBuilder()
-                .setId(id)
-                .setP(ByteString.copyFrom(p.toByteArray()))
-                .setQ(ByteString.copyFrom(q.toByteArray()))
-                .setH(ByteString.copyFrom(h.toByteArray()))
-                .build();
-        return result;
-    }
-
-    static public StdResponse newStdResponse(int id) {
-        return StdResponse.newBuilder().setId(id).build();
-    }
-
-    static public PrimalityTestResponse newPrimalityTestResponse(int id, BigInteger v) {
-        PrimalityTestResponse result= PrimalityTestResponse.newBuilder()
-                .setId(id)
-                .setV(ByteString.copyFrom(v.toByteArray()))
-                .build();
-        return result;
-    }
-
-    static public PrimalityTestResponse newPrimalityTestResponse(int id) {
-        return PrimalityTestResponse.newBuilder().setId(id).build();
-    }
 }
